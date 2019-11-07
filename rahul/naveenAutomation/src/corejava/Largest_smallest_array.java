ff3614e6720d37b60f5f51675c8c76f5f225ef96
dell <dell@192.168.56.1>
2019-11-07 18:51:16 +0530

    finding largest number in an array

---
+++ rahul/naveenAutomation/src/corejava/Largest_smallest_array.java
@@ -0,0 +1,22 @@
+package corejava;
+
+public class Largest_smallest_array {
+	public static void main(String args[]) {
+		
+		int num[]= {10,20,50,80,635};
+		
+		int largest=num[0];
+		int smallest=num[0];
+		
+		for(int i=1;i<num.length;i++) {
+			
+			if(num[i]>largest) {
+				largest=num[i];
+			}
+			
+			
+		}
+		System.out.println(largest);
+	}
+
+}
